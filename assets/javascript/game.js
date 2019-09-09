8b2dd7912a8fa7a7dfa5295fa9881a695eb9dd73
Sergo Mikirtumov <mikirtumov@gmail.com>
2019-09-08 23:03:34 -0400

    saving

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+var wins =0;
+var lose =0;
+var guesses =9;
+var guessed =[];
+
+
+
+
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+var compGuess = Math.floor(Math.random() * computerChoices.length);
+
+var compGuessLetter = computerChoices[compGuess];
+
+console.log(compGuess);
+console.log(compGuessLetter);
+
+
+
+function restart(){
+     guesses =9;
+     guessed =[];
+     
+}
+document.onkeyup = function(event) {
+
+      var userGuess = event.key;
+      guessed.push(userGuess);
+      
+      document.getElementById("guessed").innerHTML=guessed;
+      guesses--;
+      
+      
+      document.getElementById("guesses").innerHTML=guesses;
+      
+      if (guesses == 0){
+        lose++;
+        document.getElementById("lose").innerHTML=lose;
+        restart();
+      }
+      else if (userGuess == compGuessLetter){
+        wins++;
+        document.getElementById("wins").innerHTML=wins;
+        restart();
+      }
+}
