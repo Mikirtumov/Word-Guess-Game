331429d307a5ab5a7a1f3664e1fdc517f621d6c1
Sergo Mikirtumov <mikirtumov@gmail.com>
2019-09-09 22:00:02 -0400

    saving

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,23 +9,25 @@ var guessed =[];
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
-var compGuess = Math.floor(Math.random() * computerChoices.length);
+var compGuess = Math.floor(Math.random() * computerChoices.length) + 1;
 
 var compGuessLetter = computerChoices[compGuess];
 
 console.log(compGuess);
 console.log(compGuessLetter);
 
-
+function restartGuess(){
+  Math.floor(Math.random() * computerChoices.length) + 1;
+}
 
 function restart(){
-     guesses =9;
-     guessed =[];
-     
+     guesses = 9;
+     guessed =[];   
+     restartGuess();
 }
 document.onkeyup = function(event) {
 
-      var userGuess = event.key;
+      var userGuess = event.key.toLowerCase();
       guessed.push(userGuess);
       
       document.getElementById("guessed").innerHTML=guessed;
@@ -34,12 +36,12 @@ document.onkeyup = function(event) {
       
       document.getElementById("guesses").innerHTML=guesses;
       
-      if (guesses == 0){
+      if (guesses === 0){
         lose++;
         document.getElementById("lose").innerHTML=lose;
         restart();
       }
-      else if (userGuess == compGuessLetter){
+      else if (userGuess === compGuessLetter){
         wins++;
         document.getElementById("wins").innerHTML=wins;
         restart();
